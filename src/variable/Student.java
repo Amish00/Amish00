5f7d36c3d7a23e1669975eb186f4ef01cfbc3f01
amish00 <menastla8@gmail.com>
2023-05-29 08:58:34 +0545

    variable class

---
+++ src/variable/Student.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Student {
+    int number = 50; // instance variable
+    static  String collegeName = "VIRINCHI"; // static variable
+
+    public void method(){
+        int a = 10;// local variable
+        System.out.println("Local variable = " + a);
+        System.out.println("Instance variable = " + number);
+
+
+    }
+
+}
