02fb7c3adec639e57df394780dc7a2cfff101d5a
Amish00 <menastla8@gmail.com>
2023-06-12 08:51:49 +0545

    Scanner Practice

---
+++ src/controlstatement/scanner/college.java
@@ -0,0 +1,25 @@
+package controlstatement.scanner;
+
+import java.util.Scanner;
+
+public class college {
+    Scanner sc = new Scanner(System.in);
+
+    public void getStudentGrade() {
+        System.out.println("Enter Math Marks: ");
+        int mathMarks = sc.nextInt();
+
+        System.out.println("Enter Science Marks: ");
+        int scienceMarks = sc.nextInt();
+
+        if (mathMarks >= 80 && scienceMarks >= 80) {
+            System.out.println("You have got Distinction.. ");
+        } else if (mathMarks >= 60 && scienceMarks >= 60) {
+            System.out.println("You have got First Division.. ");
+        } else if (mathMarks >= 35 && scienceMarks >= 35) {
+            System.out.println("You have passed.. ");
+        } else {
+            System.out.println("You have failed..");
+        }
+    }
+}
---
+++ src/controlstatement/scanner/collegeOutput.java
@@ -0,0 +1,8 @@
+package controlstatement.scanner;
+
+public class collegeOutput {
+    public static void main(String[] args) {
+        college col = new college();
+        col.getStudentGrade();
+    }
+}
