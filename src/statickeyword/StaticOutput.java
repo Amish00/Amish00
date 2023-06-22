66940cc0b6e539b497cbe39c8529343885903552
Amish00 <menastla8@gmail.com>
2023-06-22 08:30:22 +0545

    Static Keyword

---
+++ src/statickeyword/StaticOutput.java
@@ -0,0 +1,7 @@
+package statickeyword;
+
+public class StaticOutput {
+    public static void main(String[] args) {
+        System.out.println("Area of Cube = " + StaticExample.getAreaOfCube(5));
+    }
+}
