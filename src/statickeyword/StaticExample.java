2d65b6d68b8695305ceff2de59b639457240d195
Amish00 <menastla8@gmail.com>
2023-06-22 08:45:39 +0545

    Static Keyword

--- src/statickeyword/StaticExample.java
+++ src/statickeyword/StaticExample.java
@@ -4,4 +4,9 @@ public class StaticExample {
     public static int getAreaOfCube(int a){
         return a*a*a;
     }
+
+    public static float getInterest(float p, float t, float r){
+        float si = (p*t*r)/100;
+        return si;
+    }
 }
--- src/statickeyword/StaticOutput.java
+++ src/statickeyword/StaticOutput.java
@@ -1,7 +1,21 @@
 package statickeyword;
 
+import java.util.Scanner;
+
 public class StaticOutput {
     public static void main(String[] args) {
         System.out.println("Area of Cube = " + StaticExample.getAreaOfCube(5));
+        System.out.println();
+        System.out.println("Enter p, t, r");
+//        System.out.println("Enter principal");
+        Scanner sc = new Scanner(System.in);
+        float p = sc.nextFloat();
+//        System.out.println("Enter time");
+        float t = sc.nextFloat();
+//        System.out.println("Enter rate");
+        float r = sc.nextFloat();
+
+        System.out.println(StaticExample.getInterest(p,t,r));
     }
 }
+
