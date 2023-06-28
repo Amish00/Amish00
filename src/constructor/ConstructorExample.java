8d57772aecdf246330c7f378ac550159cd447e0f
Amish00 <menastla8@gmail.com>
2023-06-28 07:54:02 +0545

    ConstructorExample

---
+++ src/constructor/ConstructorExample.java
@@ -0,0 +1,25 @@
+package constructor;
+
+public class ConstructorExample {
+    public ConstructorExample(){
+        System.out.println("Non parameterized constructor...");
+    }
+
+    public ConstructorExample(String name){
+        System.out.println(" parameterized constructor");
+    }
+
+    public void getName(){
+        System.out.println("Virinchi");
+    }
+    public void getName(String name){
+        System.out.println("Virinchi2");
+    }
+    public static void main(String[] args) {
+
+        ConstructorExample e1 = new ConstructorExample();
+        ConstructorExample e2 = new ConstructorExample("Ram");
+        e1.getName();
+        e1.getName("Sita");
+    }
+}
