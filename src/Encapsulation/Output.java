49d5fc18fa4d27f10c6a786d59a6658cca2e26a3
Amish Man Joshi <97604420+Amish00@users.noreply.github.com>
2023-08-01 07:48:30 +0545

    encapsulation

---
+++ src/Encapsulation/Output.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Output {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setId(110);
+        s.setName("Virinchi");
+        System.out.println(s.getId());
+        System.out.println(s.getName());
+    }
+}
